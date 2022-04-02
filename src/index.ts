7db8d6c9d2615be599783d359bc36a4ee32e2c06
byun618 <byun618@gmail.com>
2022-04-02 18:27:18 +0900

    코드 수정

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,22 @@ import http from 'http'
 import { Server } from 'socket.io'
 import { initApp } from './express-app'
 
-const PORT = process.env.APP_PORT || 3002
+const PORT = process.env.APP_PORT || 3001
 
 const serve = async () => {
   const app = initApp()
   const server = http.createServer(app)
   const io = new Server(server, {
     cors: { origin: '*', methods: ['GET', 'POST'] },
+    path: `/${process.env.NAMESPACE}`,
   })
 
-  const ns = io.of(`/${process.env.NAMESPACE}`)
-
-  ns.on('connection', (socket) => {
+  io.on('connection', (socket) => {
     console.log('connect')
+
+    socket.on('disconnect', () => {
+      console.log('disconnect')
+    })
   })
 
   server.listen(PORT, () => {
