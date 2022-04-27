e0b7b4d3a7436ae26012ed2f659e019c3cc8c98a
byun618 <byun618@gmail.com>
2022-04-27 22:56:23 +0900

    배포 자동화

---
+++ build_deploy.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+version=$1
+
+echo "Set Version: ${version}"
+
+echo "Build Start"
+docker build -t "byun618/auto-trade-program:${version}" .
+
+echo "Push Start"
+docker push "byun618/auto-trade-program:${version}"
+
+echo "Rolling Update Start"
+kubectl set image deployment/auto-trade-program-byun618-1 auto-trade-program-byun618-1=byun618/auto-trade-program:${version}
+
+echo "END "
