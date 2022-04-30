aa9482cc9997bca31c2f17262d4e7796ffa65bfc
byun618 <byun618@gmail.com>
2022-04-30 17:21:19 +0900

    v0.0.4

--- build_deploy.sh
+++ build_deploy.sh
@@ -11,5 +11,6 @@ docker push "byun618/auto-trade-program:${version}"
 
 echo "Rolling Update Start"
 kubectl set image deployment/auto-trade-program-byun618-1 auto-trade-program-byun618-1=byun618/auto-trade-program:${version}
+kubectl set image deployment/auto-trade-program-byun618-2 auto-trade-program-byun618-2=byun618/auto-trade-program:${version}
 
 echo "END "
